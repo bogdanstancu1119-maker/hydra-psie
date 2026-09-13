9f779b741f713a05092d6b93ce3eab78c2be3bb8
bogdanstancu1119-maker <bogdanstancu1119@gmail.com>
2026-09-13 13:30:38 -0300

    Create One-liner.sh for Termux installation
    Add a one-liner script for Termux installation

---
+++ meta-megaforma/Scripts/One-liner.sh
@@ -0,0 +1,4 @@
+# ONE-LINER pentru instalare Termux
+# Copiază și rulează în Termux:
+
+curl -fsSL https://raw.githubusercontent.com/bogdanstancu1119-maker/hydra-psie/main/scripts/termux-install.sh | bash
